24fe4ef5372f16c83518d8ac9ced3f6b736ae796
Boris <boriza@users.noreply.github.com>
2019-06-16 18:22:47 +1000

    initial

---
+++ train.py
@@ -0,0 +1,12 @@
+from azureml.core import Run
+from matplotlib import pyplot as plt
+
+run = Run.get_context()
+Foo = [1,2,3,4]
+Bar = [4,3,2,1]
+
+plt.title('Foo vs Bar')
+plt.plot(Foo, label='Foo')
+plt.plot(Bar, '-r', label='Bar')
+
+run.log_image('Plot', plot= plt)
